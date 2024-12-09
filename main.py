ffd5463ca989c30764696c8dfb846545bf60f098
abdimk <abdisamk@gmail.com>
2024-12-09 13:12:54 +0300

    fixing api

---
+++ main.py
@@ -0,0 +1,46 @@
+import asyncio
+import sys
+import re 
+
+from slang.api import DuckChat,model_type
+
+M = model_type.ModelType
+
+async def typeeffect(text: str, delay: float = 0.04):
+    for char in text:
+        sys.stdout.write(char)
+        sys.stdout.flush()
+        
+        await asyncio.sleep(delay)
+    
+    sys.stdout.write("\n")
+
+
+# trying to remove the ** starts that look a annoying as hell
+def remove_markdown_formatting(text: str) -> str:
+    text = re.sub(r'\*\*(.*?)\*\*', r'\1', text) 
+    text = re.sub(r'\*(.*?)\*', r'\1', text)   
+
+    return text
+
+
+Custom_query = {
+    'prompt':'',
+    "filter":"",
+    "exporeHttp":'',
+    "systemDate":"",
+    "disabledTopics":[],
+    "transformerModels":""
+}
+
+
+async def main(question: str = "") -> str:
+    print(f"Question: {question}")
+    async with DuckChat(M.GPT4o_Mini) as d1:
+        response = await d1.ask_question(question)
+        response = remove_markdown_formatting(response)
+        await typeeffect(response,0.02)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
--- slang/models/model_type.py
+++ slang/models/model_type.py
@@ -2,11 +2,11 @@ from enum import Enum
 
 
 class ModelType(Enum):
-    GPT4o = "gpt-4o-mini"
-    GPT4o = "gpt-4o",
-    GPT35oTurbo = "gpt3.5-turbo"
+    GPT4o_Mini = "gpt-4o-mini"
+    GPT4o_Full = "gpt-4o",
+    GPT35o_Turbo = "gpt3.5-turbo"
     Claude = "claude-3-haiku-20240307"
-    Llama = "meta-llama/Meta-Llama-3.1-70B-Instruct-Turbo"
+    Llama3o1 = "meta-llama/Meta-Llama-3.1-70B-Instruct-Turbo"
     Mixtral = "mistralai/Mixtral-8x7B-Instruct-v0.1"
 
 
