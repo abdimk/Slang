496bc06c426ff1aae7cf3bee87b33aabc73515df
abdimk <abdisamk@gmail.com>
2024-12-09 07:05:54 +0300

    initial commit

---
+++ slang/api.py
@@ -0,0 +1,12 @@
+from types import TracebackType
+from typing import AsyncGenerator
+
+import aiohttp
+import msgspec
+from fake_useragent import UserAgent
+## Import the exceptions
+
+
+from .models.model_type import ModelType
+
+
